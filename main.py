0115df1ed9a24ef1273f18add06b392567090ca6
Giorgio Salvemini <giorgio@salvemini.no>
2024-10-03 13:27:18 +0200

    Added base for convenience entrypoint

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Allows the user to run `morning_greetings` by calling `python main.py`, instead of executing the
+module directly
+"""
+
+
+from morning_greetings import ContactsManager, ImportMode, generate_message, send_message
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
