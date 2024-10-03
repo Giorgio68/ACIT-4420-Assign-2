27089058c41a8add1f385fba2829b8f6cfc6fc24
Giorgio Salvemini <giorgio@salvemini.no>
2024-10-03 12:52:28 +0200

    Added message sender and relevant tests

---
+++ morning_greetings/message_sender.py
@@ -0,0 +1,22 @@
+"""
+This module simulates sending messages to a given email address, by printing the message to
+console
+"""
+
+
+def send_message(email: str, message: str) -> None:
+    """
+    "Sends" the message to the provided email address
+
+    :param email: The email address of the recipient
+    :param message: The body of the email
+    """
+
+    if not email:
+        raise ValueError("No email address was provided")
+
+    if not message:
+        raise ValueError("No message body was provided")
+
+    # Simulate sending a message (replace this with actual email sending logic if needed)
+    print(f"Sending message to {email}: {message}")
---
+++ morning_greetings/tests/test_message_sender.py
@@ -0,0 +1,15 @@
+"""
+Tests the `message_sender` module
+"""
+
+import unittest
+from ..message_sender import send_message
+
+
+class TestContactManager(unittest.TestCase):
+    def test_sender(self):
+        send_message("test@email.org", "Hello")
+
+        # make sure that an exception is raised if an empty email address or body is provided
+        self.assertRaises(ValueError, send_message, "test@email.org", "")
+        self.assertRaises(ValueError, send_message, "", "Hello")
