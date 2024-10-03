cba481dc222bc066b4fd46249d6bda29f4832786
Giorgio Salvemini <giorgio@salvemini.no>
2024-10-03 11:32:17 +0200

    Added `__init__` and `__main__` to module

---
+++ morning_greetings/__init__.py
@@ -0,0 +1,19 @@
+"""
+The `morning_greetings` module can be used to automate greeting a list of contacts. It will
+generate a message for each user it is given, and send them a personalized greeting.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Giorgio Salvemini (s351995@oslomet.no)"
+
+
+from .contact_manager import *
+from .logger import *
+from .message_generator import *
+from .message_sender import *
+
+# define dunder all to allow importing classes
+__all__ = [
+    "ContactsManager",
+    "ImportMode",
+]
